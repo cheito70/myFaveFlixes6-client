f0542668165581040a507351907ff5e6c7cecdbe
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-12-07 13:54:10 -0600

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@ import React, { useState }  from 'react';
 import {Form, Col, Row, Container, Card, CardGroup, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 import './login-view.scss';
 
@@ -9,25 +10,57 @@ export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
 
+    //declare a hook for each input
+    const [usernameErr, setUsernameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
+
+    //validate user inputs
+    const validate = () => {
+        let isReq = true;
+
+        setUsernameErr('');
+        setPasswordErr('');
+
+        if (!username) {
+            setUsernameErr("Username is required.");
+            isReq = false;
+        } else if (username.length < 3) {
+            setUsernameErr("Username must be at least 3 characters long.");
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr("Password is required.");
+            isReq = false;
+        } else if (password.length < 6) {
+            setPasswordErr("Password must be at least 6 characters long.");
+            isReq = false;
+        }
+        return isReq;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        const isReq = validate();
+        if (isReq) {
         /* Send a request to the server for authentication*/
         axios.post( 'https://myfaveflixes.herokuapp.com/login', {
         Username: username,
         Password: password
         })
-        .then(response => {
+        .then((response) => {
             const data = response.data;
             props.onLoggedIn(data);
         })
-        .catch(e => {
-            console.log('no such user')
+        .catch((err) => {
+            console.log('wrong username or password!!!' + err);
         });
+       }
     };
 
     const handleRegisterClick = (e) => {
         e.preventDefault();
-        props.toRegister();
+        console.log("clicked Register");
+        props.toggleRegister(false);
     };
 
 
@@ -45,18 +78,30 @@ export function LoginView(props) {
                             <Form className="login-border">
                                 <Form.Group controlId="formUsername">
                                     <Form.Label>Username:</Form.Label>
-                                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                    <Form.Control 
+                                    type="text" 
+                                    placeholder="Username Here" 
+                                    value={username}
+                                    onChange={e => setUsername(e.target.value)} />
+                                    {/* validation errors display here*/}
+                                    {usernameErr && <p className="alert alert-danger">{usernameErr}</p>}
                                 </Form.Group>
 
                                 <Form.Group controlId="formPassword">
                                     <Form.Label>Password:</Form.Label>
-                                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                                    <Form.Control 
+                                    type="password" 
+                                    placeholder="Password Here"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)} />
+                                    {/* validation errors display here */}
+                                    {passwordErr && <p className="alert alert-danger">{passwordErr}</p>}
                                 </Form.Group>
                                 <Button 
                                 className="login-button mt-2 mr-2"
-                                variant="primary" 
+                                variant="success" 
                                 type="submit" 
-                                onClick={handleSubmit}>Submit</Button>
+                                onClick={handleSubmit}>Login</Button>
 
                                 <Button 
                                 className="register-button mt-2" 
@@ -70,6 +115,12 @@ export function LoginView(props) {
                 </Col>
             <Col></Col>
         </Row>
+        <Row className="justify-content-center m-1">
+        <Link to={"/register"}>
+          {''}
+          <a className="text-muted">Click here to register a new account.</a>
+        </Link>
+      </Row>
     </Container>
     );
 }
