76309e037c888e1526e1e9358682cea1f9b1553b
cheito70 <49110712+cheito70@users.noreply.github.com>
2023-01-03 16:14:54 -0600

    Updated ML and filter

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 
 function VisibilityFilterInput(props) {
     return (
