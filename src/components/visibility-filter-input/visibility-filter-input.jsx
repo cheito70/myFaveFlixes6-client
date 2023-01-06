19f519162cd493cf725a0195c7ebf959ca44ca63
cheito70 <49110712+cheito70@users.noreply.github.com>
2023-01-06 15:19:36 -0600

    Update visibility-filter-input.jsx

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -6,7 +6,7 @@ import { setFilter } from '../../actions/actions';
 
 function VisibilityFilterInput(props) {
     return (
-        <Col>
+        <Col md={8}>
         <Form.Control
         onChange={(e) => props.setFilter(e.target.value)}
         value={props.visibilityFilter}
